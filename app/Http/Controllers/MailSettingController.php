3b86e14e95f7317d484cd46cc329d485fbe68938
Ilya Malygin <malygin.ilja@yandex.ru>
2021-11-03 15:51:50 +0300

    refractoring

--- app/Http/Controllers/MailSettingController.php
+++ app/Http/Controllers/MailSettingController.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+// declare(strict_types=1);
 namespace App\Http\Controllers;
 use App\Models\{Contact, Answer};
 use App\Mail\{MailClass, MailToPeopleClass};
@@ -19,26 +19,13 @@ class MailSettingController extends Controller{
 
     //Функция отправки отзыва от пользователя
     public function sendRiview(ContactRequest $request){
-
       $Review = new Contact();
-      $email = $Review->email = $request->input('email');
-      $msg = $Review->message = $request->input('msg');
-      $name = $Review->name = $request->input('name')   ;
-      $topic = $Review->topic = $request->input('topic');
+      $Review->takeData($request);
+      return redirect('/');
+    }
 
-      // Проверка email на повторную отпрывку отзыва
-      if (Contact::where('email', $email)->first()){    //Если пользователь с таким email существует
-          $userReviewDate = new DateTime((string) Contact::where('email',$email)->latest()->value('created_at'));   //То берем последнюю дату его оразения
-          $nowDate = new DateTime();    //Заводим переменную с текущим временем
-          $diffInHours = $userReviewDate->diff($nowDate);    // сравниваем их по датам
-          if(($diffInHours->days * 24) + $diffInHours->h <= 24){    //получаем разницу дат в часах и сравниваем
-              return redirect('/')->with('timeerror', 'Вы уже отправляли сообщение.Обратитесь позднее');
-          }
-        }
 
-      $Review->save();
-      return $this->takeParamsForMail($name, $email, $topic, $msg) ;
-      }
+
 
       //Функция ответа на отзыв
       public function sendReplyToRiview(Request $request ,int $statusId ){
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,15 +1,73 @@
 <?php
-declare(strict_types=1);
+// declare(strict_types=1);
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use DateTime;
+use App\Http\Requests\ContactRequest;
 class Contact extends Model
 {
     use HasFactory;
+    public $request;
+
+    //получаем обдин объект через Contact
     public function gimmeAnswer(){
-        return $this->hasOne(Answer::class);  //Пoлучить обдин обект через Contact
+        return $this->hasOne(Answer::class);
+    }
+
+    //ищем emal в БД
+    public function findEmail($email){
+        return $this::where('email', $email)->first();
+    }
+
+    //получаем последнуу значение даты отзыва,если он найден
+    public function getLatestEmailsDate($email){
+      return $this::where('email',$email)->latest()->value('created_at');
+    }
+
+    //проверяем Email пользователя
+    public function checkEmailTime($email){
+
+      if ($this->findEmail($email)){    //Если пользователь с таким email существует
+          $userReviewDate = new DateTime((string) ($this->getLatestEmailsDate($email)));   //То берем последнюю дату его оразения
+          $nowDate = new DateTime();    //Заводим переменную с текущим временем
+          $diffInHours = $userReviewDate->diff($nowDate);    // сравниваем их по датам
+          if(($diffInHours->days * 24) + $diffInHours->h <= 24){    //получаем разницу дат в часах и сравниваем
+              return false;
+          }
+        }
+          return true;
+    }
+
+    //Показать сообщение об успешной отправке
+    public function redirectToWithSuccess(){
+      return redirect('/')->with('success', 'Сообщение отпрвалено');
+    }
+
+    //показать сообщение об обшибке
+    public function redirectToWithError(){
+      return redirect('/')->with('timeerror', 'Вы уже отправлли сообщение');
+    }
+
+    //функция сохранения данных формы при успешной проверке
+    public function saveMessage($email){
+    if($this->checkEmailTime($email)){
+          $this->save();
+          $this->redirectToWithSuccess();
+        }else{
+    $this->redirectToWithError();
+    }
+    }
+
+    //функция принятия данных
+    public function takeData($request){
+
+      $email = $this->email = $request->email;
+      $msg  = $this->message = $request->msg;
+      $name = $this->name = $request->name;
+      $topic = $this-> topic = $request->topic;
+      $this->saveMessage($email);
 
     }
+
 }
